a5d4bfca6a3b8e395f4749ab29a6ec5c40ad41a8
ChrisAE13 <christina.papachristoudi@gmail.com>
2023-11-24 00:24:56 +0200

    added comments

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,32 +1,29 @@
 import os
 import config
-from pathlib import Path
-from llama_hub.file.unstructured import UnstructuredReader
 from llama_index import(
-    SimpleDirectoryReader, 
-    VectorStoreIndex,
     StorageContext,
     ServiceContext,
     load_index_from_storage,
 ) 
 from llama_index.llms import OpenAI
 
-# os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = "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"
+# get OPENAI key from system vars
+# use os.environ["OPENAI_API_KEY"] = "..." to use other key
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
 
 def promt(): 
     print('\n>> ', end='') 
 
+# load index from storage
 storage_context = StorageContext.from_defaults(persist_dir=config.STORAGE_DIR)
 index = load_index_from_storage(storage_context)
 
-
+# set llm model and query engine
 llm = OpenAI(temperature=config.TEMPERATURE, model=config.MODEL)
 service_context = ServiceContext.from_defaults(llm=llm)
 query_engine = index.as_query_engine(service_context=service_context)
 
-
-
+# chatbot loop
 print('\nHello! Ask me about HY335! (Press q, quit or exit to exit.)')
 promt()
 
--- src/index_and_store.py
+++ src/index_and_store.py
@@ -1,26 +1,35 @@
 import os
 import config
-from pathlib import Path
+# from pathlib import Path
 from llama_index.llms import OpenAI
-from llama_hub.file.unstructured import UnstructuredReader
 from llama_index import (
 	SimpleDirectoryReader, 
 	VectorStoreIndex,
 	ServiceContext,
 )
 
-# os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = "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"
+# get OPENAI key from system vars
+# use os.environ["OPENAI_API_KEY"] = "..." to use other key
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
 
 llm = OpenAI(temperature=config.TEMPERATURE, model=config.MODEL)
 service_context = ServiceContext.from_defaults(llm=llm)
 
-# TODO:
-dir_reader = SimpleDirectoryReader(config.DATA_DIR, file_extractor={
-	".html": UnstructuredReader(),
-	".txt": UnstructuredReader(),
-})
+# clear storage director
+if(os.path.exists(config.STORAGE_DIR)):
+	print("Clearing", config.STORAGE_DIR, "directory")
+	for file in os.listdir(config.STORAGE_DIR):
+		os.remove(config.STORAGE_DIR+'/'+file)
+		print(file, "deleted")
+
+# TODO: maybe find a more efficient way of reading and storing info
+# perhaps diff loaders for diff file types --> see llama_hub
+dir_reader = SimpleDirectoryReader(config.DATA_DIR)
 documents = dir_reader.load_data()
 
+
+# TODO: check for different type of indexing instead of vector
+# check extra parameters for indexing
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 index.storage_context.persist(persist_dir=config.STORAGE_DIR) 
+print("Created storage directory:", config.STORAGE_DIR)
